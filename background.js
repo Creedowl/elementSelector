d11c1d7c08d9cd133db526cf2b60363581f41b3d
queensferry <queensferry.me@gmail.com>
2019-01-29 11:35:26 +0800

    rm: chrome commands api

--- background.js
+++ background.js
@@ -2,7 +2,3 @@ chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.insertCSS({ file: 'selector.css' });
     chrome.tabs.executeScript({ file: 'selector.js' });
 });
-
-chrome.commands.onCommand.addListener(command => {
-    console.log(command);
-})
