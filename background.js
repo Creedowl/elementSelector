fd5bfadcb09f30344643d34ca05d7e87f17f5498
queensferry <queensferry.me@gmail.com>
2019-01-29 11:19:54 +0800

    fix: keyboard shortcut operation enabled

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    console.log(tab);
     chrome.tabs.insertCSS({ file: 'selector.css' });
     chrome.tabs.executeScript({ file: 'selector.js' });
-  });
-  
+});
+
+chrome.commands.onCommand.addListener(command => {
+    console.log(command);
+})
